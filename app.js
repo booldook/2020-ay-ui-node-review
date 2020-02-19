57e4e9f1d56cf9962150a4b1454547aaef90ccec
booldook <booldook@gmail.com>
2020-02-19 14:31:16 +0900

    top.pug

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.set("views", "./views");
 
 /* 라우터 세팅 */
 app.use("/", express.static("./public"));
+app.use("/", (req, res, next) => {
+	res.render("index.pug", {file: "index"});
+});
 app.use("/member", memberRouter);
 app.use("/mypage", mypageRouter);
 app.use("/board", boardRouter);
