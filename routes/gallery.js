89904a640cb0080cda68be697f0909688ee32da5
booldook <booldook@gmail.com>
2020-02-20 10:04:35 +0900

    list.pug 완성

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+router.get(["/", "/list"], (req, res, next) => {
+	const value = {
 
+	};
+	res.render("gallery/list.pug", value);
+});
+
+router.get("/write", (req, res, next) => {
+	const value = {
+	
+	};
+	res.render("gallery/write.pug", value);
+});
 
 module.exports = router;
