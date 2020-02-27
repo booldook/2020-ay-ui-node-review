d324a10c38b218b2eae8a27fe9ce984b002b0eb7
booldook <booldook@gmail.com>
2020-02-27 10:09:28 +0900

    list.pug

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -5,3 +5,8 @@ var $grid = $('.grid').imagesLoaded(function () {
 		percentPosition: true
 	});
 });
+
+function onDelete(id) {
+	var chk = confirm("정말로 삭제하시겠습니까? \n삭제한 데이터는 복구되지 않습니다.");
+	if(chk) location.href = '/gallery/delete/'+id;
+}
